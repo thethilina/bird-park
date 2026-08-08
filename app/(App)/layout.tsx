41806ade252ec3613e05125d6b00b79d357e870e
Thilina Dewmina <thilinadewmina7@gmail.com>
2026-08-08 19:13:34 +0530

    mobile top nav bar

--- app/(App)/layout.tsx
+++ app/(App)/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import "../globals.css";
 import NavBar from "@/public/components/NavBar";
 import MoobileNav from "@/public/components/MobileNav";
-
+import MobileTopNavbar from "@/public/components/MobileTopNavbar";
 export const metadata: Metadata = {
   title: "Bird Park",
   description: "A sanctuary for artists.",
@@ -19,6 +19,7 @@ export default function AppLayout({
     <div className="flex flex-1">
       <div className="flex-1">
         <NavBar />
+        <MobileTopNavbar />
         <div className="xl:px-15 lg:px-10 md:px-5 sm:px-2 ">
           {children}
         </div>
--- public/components/MobileTopNavbar.tsx
+++ public/components/MobileTopNavbar.tsx
@@ -19,12 +19,10 @@ import { useRouter } from 'next/navigation'
 import { FaUserFriends } from "react-icons/fa";
 import { FaConnectdevelop } from "react-icons/fa";
 import NotificationDropdown from "@/public/components/NotificationDropdown";
-import { RiGalleryLine } from "react-icons/ri";
-import { RiUserCommunityFill } from "react-icons/ri";
 
 
 
-function NavBar() {
+function MobileTopNavbar() {
   const { user } = useAuth();
   const [isDropdownOpen, setIsDropdownOpen] = useState(false);
   const [isDarkMode, setIsDarkMode] = useState(false);
@@ -93,42 +91,58 @@ function NavBar() {
 
   return (
     
-    <nav className="fixed py-5  bottom-0 sm:hidden   z-50 px-10 bg-(--color-background) dark:bg-(--background)  border-(--border)  lg:pt-5  w-full  border-b-2   lg:py-3   flex items-center justify-between ">
+    <nav className="sticky sm:hidden xl:px-15 lg:px-10 md:px-5 px-5   z-50 py-3  bg-(--color-background) dark:bg-(--background)  border-(--border)    w-full top-0 border-b-2    flex items-center justify-between ">
 
 
     {/** right side */}
+    <div className="flex  items-center  gap-x-4 lg:gap-x-8">
 
+    <Image src={logo} alt="birdpark logo" className="lg:w-9 w-7 hidden dark:block " />     
+    <Image src={logolight} alt="birdpark logo" className="lg:w-10 w-7 dark:hidden " />
 
-   <Link href={"/"}>  <RiGalleryLine  className="dark:text-[#BEC9F4]  transition-transform hover:scale-105 size-6 lg:size-7" /></Link>
-     
-        <Link href="/Create/Submitwork">
-      <FaRegPlusSquare className=" dark:text-[#BEC9F4] transition-transform hover:scale-105  size-6 lg:size-7" />
-    </Link>
+    <ul className=" items-center gap-x-5 text-2xl hidden lg:flex">
+   <Link href={"/"}><li>Board</li></Link>
+      <Link href={"/Circle"}><li>Circles</li></Link>
+    </ul>
 
-    
-      <Link href={"/Circle"}><RiUserCommunityFill className="dark:text-[#BEC9F4]  transition-transform hover:scale-105 size-6 lg:size-7" /></Link>
+    </div>
 
+    {/** search bar */}
 
+    <div className="relative ">
+      <IoMdSearch className="absolute right-3 top-1 size-7  " />
+      <input placeholder="Explore..." type="text" className="font-sans bg-(--colorbg) dark:bg-(--colorbgdark) border border-(--border) dark:border-(--borderdark)   rounded-full py-1.5 px-4" />
+    </div>
 
-
-   
-    
     {/** left side */}
+    <div className="flex items-center gap-x-5 lg:gap-x-7 color-(--primary) dark:color-(--primarydark) ">
 
-   <Link href="/suggections">
-      <FaUserFriends  className="dark:text-[#BEC9F4]  transition-transform hover:scale-105 size-6 lg:size-7" />
-    </Link>
+ 
 
 
     
-    
+    <NotificationDropdown />
 
-
+    <Image ref={avatarRef} onClick={() => setIsDropdownOpen(!isDropdownOpen)} src={user?.profileImage || userData.avatar} width={36} height={36} alt="user avatar" className="  w-7 lg:w-9 rounded-full border border-white cursor-pointer" />
+    </div>
 
     {/** dropdown */}
-  
+    <div ref={dropdownRef} className={`absolute z-200 top-full mr-2 mt-2 right-0 font-sans  w-60  bg-(--color-background) dark:bg-(--background)     border border-(--border) dark:border-(--borderdark) rounded-lg shadow-lg py-2 ${isDropdownOpen ? 'block' : 'hidden'}`}>
+
+    <Link href={`/Profile/${user?._id}`}  onClick={()=>{setIsDropdownOpen(false)}}   >  <div  className=" flex gap-x-4  items-center   font-medium text-lg px-4 py-4 mx-2 border-b border-(--border) dark:border-(--borderdark)">
+        <Image src={user?.profileImage || userData.avatar} width={20} height={20} alt="user avatar" className="w-8 h-8 transition-transform hover:scale-105  rounded-full border border-white " />
+        <p className="  ">{user?.fullName || "Guest User"}</p>
+      </div>          </Link> 
+
+     <div className="px-2 py-2 text-md  " >
+      <Link onClick={()=>{setIsDropdownOpen(false)}} href="/Profile/Settings" className="block px-4 py-2  hover:bg-(--hover) dark:hover:bg-(--hoverdark) rounded"> 
+        <IoIosSettings className="inline mr-2" size={20}  />Settings</Link>
+      <button onClick={handleLogout} className="block px-4 py-2  hover:bg-(--hover) hover:cursor-pointer dark:hover:bg-(--hoverdark) rounded">
+        <IoLogOut className="inline mr-2 " size={20} />Logout</button>
+    </div>
+    </div>
     </nav>
   )
 }
 
-export default NavBar
+export default MobileTopNavbar
